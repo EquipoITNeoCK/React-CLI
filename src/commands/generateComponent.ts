1a331f781e82824d8f59c7db2ce250a2a4b9bc59
julioneoCK <julio.macias@neock.es>
2024-09-10 12:20:10 +0200

    feat: modify file text, component naming and file naming

--- src/commands/generateComponent.ts
+++ src/commands/generateComponent.ts
@@ -1,48 +1,79 @@
 import fs from "fs-extra";
 import path from "path";
+import consoleCreated from "../utils/console-created.js";
 import {
   formatComponentName,
   formatDirectoryName,
+  formatFileName,
 } from "../utils/formatUtils.js";
-import consoleCreated from "../utils/console-created.js";
+import { select } from "@inquirer/prompts";
 
-export function generateComponent(name: string) {
-  const formattedName = formatComponentName(name);
+export async function generateComponent(name: string) {
+  const formatedFileName = formatFileName(name);
+  const formatedComponentName = formatComponentName(name);
   const formattedDir = formatDirectoryName(name);
+
   let componentDir = path.join(process.cwd(), formattedDir);
 
   if (!process.cwd().includes(path.join("src"))) {
     componentDir = path.join(process.cwd(), "src", formattedDir);
   }
 
+  const stylesFileInputType = await select<string>({
+    message: "¿Que tipo de fichero de estilos quieres?",
+    choices: ["plain css", "css modules", "mui styles"],
+  });
+
   fs.ensureDirSync(componentDir);
 
-  generateComponentTsx(formattedName, componentDir);
-  generateComponentStyles(formattedName, componentDir);
+  generateComponentTsx(formatedComponentName, componentDir, formatedFileName);
+
+  generateComponentStyles(formatedFileName, componentDir, stylesFileInputType);
 }
 
-function generateComponentTsx(componentName: string, componentDir: string) {
-  const componentTsx = `import {} from './${componentName}.styles';
-export default function ${componentName}() {
+function generateComponentTsx(
+  componentName: string,
+  componentDir: string,
+  fileName: string
+) {
+  const componentTsx = `function ${componentName}() {
   return (
-    <>${componentName}</>
+    <div>${componentName}</div>
   );
-};`;
+};
 
-  fs.writeFileSync(
-    path.join(componentDir, `${componentName}.tsx`),
-    componentTsx
-  );
-  consoleCreated(`${componentName}.tsx`);
+export default ${componentName};
+`;
+
+  fs.writeFileSync(path.join(componentDir, `${fileName}.tsx`), componentTsx);
+  consoleCreated(`${fileName}.tsx`);
 }
 
-function generateComponentStyles(componentName: string, componentDir: string) {
-  const componentStyles = `import {styled} from '@mui/material';`;
+function generateComponentStyles(
+  componentName: string,
+  componentDir: string,
+  fileType: string
+) {
+  let fileExtension = "";
+  let componentText = "";
+
+  switch (fileType) {
+    case "plain css":
+      fileExtension = ".css";
+      break;
+    case "css modules":
+      fileExtension = ".module.css";
+      break;
+    case "mui styles":
+      fileExtension = ".styles.ts";
+      componentText = `import { styled } from '@mui/material';`;
+      break;
+  }
 
   fs.writeFileSync(
-    path.join(componentDir, `${componentName}.styles.ts`),
-    componentStyles
+    path.join(componentDir, `${componentName}${fileExtension}`),
+    componentText
   );
 
-  consoleCreated(`${componentName}.styles.ts`);
+  consoleCreated(`${componentName}${fileExtension}`);
 }
